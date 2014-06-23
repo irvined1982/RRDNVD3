6aa22c576d571cf142621e1350606f4bb4191bb1
David Irvine <irvined@gmail.com>
2014-06-23 10:47:31 +0100

    Graph Info

--- rrdviewer/views.py
+++ rrdviewer/views.py
@@ -24,6 +24,8 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 GRAPH_PATH = '/tmp'
 
@@ -74,10 +76,58 @@ def get_graph(request, start_time, end_time, path, CF="AVERAGE"):
     return HttpResponse(json.dumps(serieses), content_type="application/json")
 
 
+def get_graph_info_dict(path):
+    info = {}
+    info_file_path = "%s.info" % path[0:-4]
+    if os.path.exists(info_file_path):
+        info = json.load(open(info_file_path))
+
+    info['rrd_absolute_path'] = path
+    info['relative_path'] = info['rrd_absolute_path']
+    if info['relative_path'].startswith(GRAPH_PATH):
+        info['relative_path'] = info['relative_path'][len(GRAPH_PATH):]
+    info['hourly_rrd_url'] = reverse('get_graph', args=[-60*60, 0, info['relative_path']])
+    info['daily_rrd_url'] = reverse('get_graph', args=[-60*60*24, 0, info['relative_path']])
+    info['weekly_rrd_url'] = reverse('get_graph', args=[-60*60*24*7, 0, info['relative_path']])
+    info['monthly_rrd_url'] = reverse('get_graph', args=[-60*60*24*31, 0, info['relative_path']])
+    info['yearly_rrd_url'] = reverse('get_graph', args=[-60*60*24*31*265, 0, info['relative_path']])
+    info['info_url'] = reverse('get_info',args=[info['relative_path']])
+
+    info['name'] = info['relative_path'][:-4]
+
+    for k, v in rrdtool.info(info['rrd_absolute_path']).iteritems():
+        components = k.split(".")
+        if len(components) == 1:
+            info[components[0]] = v
+        else:
+            key = components[0]
+            key, t, index = key.partition("[")
+            index = index.rstrip("]")
+            if key not in info:
+                info[key] = {}
+            if index not in info[key]:
+                info[key][index] = {}
+            if len(components) == 2:
+                info[key][index][components[1]] = v
+            elif len(components) == 3:
+                i = info[key][index]  # dict of third level entries
+                tkey, t, tindex = components[1].partition("[")
+                tindex = index.rstrip("]")
+                if tkey not in i:
+                    i[tkey] = {}
+                if tindex not in i[tkey]:
+                    i[tkey][tindex] = {}
+                i[tkey][tindex][components[2]] = v
+    return info
+
+
 def get_info(request, path):
     filename = os.path.join(GRAPH_PATH, *path)
     if not os.path.exists(filename):
         raise Http404("RRD does not exist")
+
+    return HttpResponse(json.dumps(get_graph_info_dict(filename)), content_type="application/json")
+
 
 def show_rrd(request, path):
     pass
@@ -92,48 +142,18 @@ def list_graphs(request):
                 id += 1
                 if id > 10:
                     continue
-                info = {}
-                info_file = "%s.info" % file[0:-4]
-                info_file_path = (os.path.join(directory, info_file))
-                if os.path.exists(info_file_path):
-                    info = json.load(open(info_file_path))
-                info['rrd_absolute_path'] = os.path.join(directory, file)
-                info['relative_path'] = info['rrd_absolute_path']
+                f=os.path.join(directory,file)
+                info = get_graph_info_dict(f)
                 info['graph_id'] = "graph_%s" % id
-                if info['relative_path'].startswith(GRAPH_PATH):
-                    info['relative_path'] = info['relative_path'][len(GRAPH_PATH):]
-                info['hourly_rrd_url'] = reverse('get_graph', args=[-60*60, 0, info['relative_path']])
-                info['daily_rrd_url'] = reverse('get_graph', args=[-60*60*24, 0, info['relative_path']])
-                info['weekly_rrd_url'] = reverse('get_graph', args=[-60*60*24*7, 0, info['relative_path']])
-                info['monthly_rrd_url'] = reverse('get_graph', args=[-60*60*24*31, 0, info['relative_path']])
-                info['yearly_rrd_url'] = reverse('get_graph', args=[-60*60*24*31*265, 0, info['relative_path']])
-                info['info_url'] = reverse('get_info',args=[info['relative_path']])
-
-                info['name'] = info['relative_path'][:-4]
-                for k, v in rrdtool.info(info['rrd_absolute_path']).iteritems():
-                    components = k.split(".")
-                    if len(components) == 1:
-                        info[components[0]] = v
-                    else:
-                        key = components[0]
-                        key, t, index = key.partition("[")
-                        index = index.rstrip("]")
-                        if key not in info:
-                            info[key] = {}
-                        if index not in info[key]:
-                            info[key][index] = {}
-                        if len(components) == 2:
-                            info[key][index][components[1]] = v
-                        elif len(components) == 3:
-                            i = info[key][index]  # dict of third level entries
-                            tkey, t, tindex = components[1].partition("[")
-                            tindex = index.rstrip("]")
-                            if tkey not in i:
-                                i[tkey] = {}
-                            if tindex not in i[tkey]:
-                                i[tkey][tindex] = {}
-                            i[tkey][tindex][components[2]] = v
-                print info
                 graphs.append(info)
+    paginator = Paginator(graphs, 10)
+    page = request.GET.get('page')
+    try:
+        graphs = paginator.page(page)
+    except PageNotAnInteger:
+        graphs=paginator.page(1)
+    except EmptyPage:
+        graphs=paginator.num_pages
+
     return render_to_response("rrdviewer/graph_list.html", {'graphs': graphs, },
                               context_instance=RequestContext(request))
